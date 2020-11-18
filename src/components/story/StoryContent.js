8676005020635bb07c4f1135850810e1be76921e
olegs7771 <olegs7771@gmail.com>
2020-11-18 19:17:11 +0200

    ok

--- src/components/story/StoryContent.js
+++ src/components/story/StoryContent.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react'
   render() {
     return (
       <div className='story__content'>
-        <h3 className="heading-3">Happy Customer</h3>
-        <h2 className="heading-2 heading-2--dark">&ldquo;The best decision of our lives  &rdquo;</h2>
+        <h3 className="heading-3 mb-sm">Happy Customer</h3>
+        <h2 className="heading-2 heading-2--dark mb-md">&ldquo;The best decision of our lives  &rdquo;</h2>
         <p className="story__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium qui veritatis neque necessitatibus .</p>
         <button className="btn">Find your own home</button>
       </div>
